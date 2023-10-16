4e345150bc1a1e951b455baa5bdc83795f7a74f8
rstr <148062120+rstr2@users.noreply.github.com>
2023-10-16 14:47:16 +0300

    Make function that creates a grid

---
+++ script.js
@@ -0,0 +1,17 @@
+const grid = document.querySelector(".grid");
+
+function makeGrid(gridSize) {
+  for(let i = 0; i < gridSize; i++) {
+    const column = document.createElement("div");
+
+    for(let j = 0; j < gridSize; j++) {
+      const cell = document.createElement("div")
+      cell.style.width = `${700 / gridSize}px`;
+      cell.style.height = `${700 / gridSize}px`;
+      column.appendChild(cell);
+    }
+    
+    grid.appendChild(column);
+  }
+}
+
