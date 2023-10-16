16cde0135965932cd3e6ac624604b0f0e85dcada
rstr <148062120+rstr2@users.noreply.github.com>
2023-10-16 15:01:14 +0300

    Add black hover effect

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const grid = document.querySelector(".grid");
 
+// Make n columns that contain n cells each
 function makeGrid(gridSize) {
   for(let i = 0; i < gridSize; i++) {
     const column = document.createElement("div");
@@ -16,3 +17,10 @@ function makeGrid(gridSize) {
   }
 }
 
+// Add a hover effect that turns the cells black when they are hovered over
+function addBlackHover() {
+  const cells = document.querySelectorAll(".cell");
+  cells.forEach(cell => {
+    cell.addEventListener('mouseover', () => cell.style.backgroundColor = 'black');
+  });
+}
